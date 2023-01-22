da6014a2beb0ca25a5eae4abac546f66f71b390c
noehernandez24 <evilld94@gmail.com>
2023-01-22 16:07:09 -0600

    CHANGE: Initial state

--- src/store/slices/userName.slice.jsx
+++ src/store/slices/userName.slice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 // Cambiamos mySlice por el nombre de nuestro slice (usersSlice, toDosSlice...)
 export const userNameSlice = createSlice({
   name: "userName",
-  initialState: "Noé",
+  initialState: "",
   reducers: {
     changeUserName: (state, action) => {
         const inputValue = action.payload
